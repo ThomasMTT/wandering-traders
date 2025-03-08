e26cd36e240af596adf55e661750f3dba3c03b21
ThomasMTT <thomasmatthys@protonmail.com>
2025-03-08 05:51:35 +0100

    [FEATURE] end trader changes (see below)
    - end trader will teleport to a random nearby location on hurt
    - end trader will destroy your loot and leave when you hit him (if he has your loot)
    - end trader will go back to the end every 2 days, he may lose some loot (33% chance)

--- common/src/main/java/life/thoms/wandering_traders/entity/EndTraderEntity.java
+++ common/src/main/java/life/thoms/wandering_traders/entity/EndTraderEntity.java
@@ -1,25 +1,37 @@
 package life.thoms.wandering_traders.entity;
 
+import life.thoms.wandering_traders.server.data.LostLootData;
 import life.thoms.wandering_traders.util.trader.EndTraderUtil;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.npc.WanderingTrader;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.Heightmap;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class EndTraderEntity extends AbstractTraderEntity {
 
     public UUID linkedPlayerUuid = null;
     public String linkedPlayerName = null;
+
     public EndTraderEntity(EntityType<? extends WanderingTrader> entityType, Level level) {
         super(entityType, level);
     }
@@ -73,6 +85,83 @@ public class EndTraderEntity extends AbstractTraderEntity {
             }
         }
         super.readAdditionalSaveData(compound);
+    }
+
+    @Override
+    public boolean hurt(DamageSource source, float amount) {
+        if (source.getEntity() instanceof Player player) {
+            if (player.isCreative() && player.isCrouching()) {
+                return super.hurt(source, getMaxHealth());
+            }
+            if (linkedPlayerUuid == player.getUUID()) {
+                if (offers != null && !offers.isEmpty()) {
+                    player.displayClientMessage(Component.translatable("end_trader_message.leave_dim_angry"), true);
+                    goBackToTheEnd();
+                }
+                return false;
+            }
+        }
+        if (source.is(DamageTypes.GENERIC_KILL)) {
+            return super.hurt(source, getMaxHealth());
+        } else {
+            randomTeleport();
+        }
+        return false;
+    }
+
+    @Override
+    public void aiStep() {
+        super.aiStep();
+        if (!isClientSide() && isRemoved()) {
+            if (linkedPlayerUuid != null) {
+                Player player = level().getPlayerByUUID(linkedPlayerUuid);
+                if (player != null) {
+                    List<ItemStack> merchantLoot = new ArrayList<>();
+                    boolean lostSomeLoot = random.nextInt(0, 100) > 33;
+                    for (MerchantOffer offer : offers.stream().toList()) {
+                        if (!lostSomeLoot || random.nextBoolean()) {
+                            ItemStack stack = offer.getResult();
+                            merchantLoot.add(stack);
+                        }
+                    }
+
+                    if (lostSomeLoot) {
+                        player.displayClientMessage(Component.translatable("end_trader_message.leave_dim_lost"), true);
+                    } else {
+                        player.displayClientMessage(Component.translatable("end_trader_message.leave_dim"), true);
+                    }
+
+                    LostLootData.PLAYER_LOST_LOOT.put(linkedPlayerUuid, merchantLoot);
+                }
+            }
+        }
+    }
+
+    public void randomTeleport() {
+        if (!level().isClientSide()) {
+            if (isAlive()) {
+                for (int attempts = 0; attempts < 15; attempts++) {
+                    double x = getX() + (random.nextDouble() - 0.5) * 64.0;
+                    double z = getZ() + (random.nextDouble() - 0.5) * 64.0;
+                    double y = getCommandSenderWorld().getHeight(Heightmap.Types.WORLD_SURFACE, (int) x, (int) z);
+                    BlockState state = getCommandSenderWorld().getBlockState(new BlockPos((int) x, (int) y - 1, (int) z));
+
+                    if (!(state.getBlock() instanceof LiquidBlock)) {
+                        playSound(SoundEvents.ENDERMAN_TELEPORT);
+                        teleportTo(x, y, z);
+                    }
+                }
+            }
+        }
+    }
+
+    public void goBackToTheEnd() {
+        if (!level().isClientSide()) {
+            if (isAlive()) {
+                playSound(SoundEvents.ENDERMAN_TELEPORT);
+                discard();
+            }
+        }
     }
 
 }
