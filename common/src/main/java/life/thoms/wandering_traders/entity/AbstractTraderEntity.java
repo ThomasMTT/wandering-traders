0ae8de5c5eff70023bfdb59b223cd77c729597ef
ThomasMTT <thomasmatthys@protonmail.com>
2025-03-08 01:50:27 +0100

    [FEATURE] create basic lost loot trades system

--- common/src/main/java/life/thoms/wandering_traders/entity/AbstractTraderEntity.java
+++ common/src/main/java/life/thoms/wandering_traders/entity/AbstractTraderEntity.java
@@ -4,7 +4,9 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.npc.WanderingTrader;
+import net.minecraft.world.item.trading.MerchantOffers;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class AbstractTraderEntity extends WanderingTrader {
 
@@ -14,6 +16,19 @@ public abstract class AbstractTraderEntity extends WanderingTrader {
 
     public static AttributeSupplier.Builder createAttributes() {
         return Villager.createAttributes();
+    }
+
+    @Override
+    protected void updateTrades() {
+    }
+
+    @Override
+    public @NotNull MerchantOffers getOffers() {
+        if (this.offers == null) {
+            this.offers = new MerchantOffers();
+            this.offers.clear();
+        }
+        return this.offers;
     }
 
 }
--- common/src/main/java/life/thoms/wandering_traders/entity/EndTraderEntity.java
+++ common/src/main/java/life/thoms/wandering_traders/entity/EndTraderEntity.java
@@ -1,13 +1,40 @@
 package life.thoms.wandering_traders.entity;
 
+import life.thoms.wandering_traders.util.trader.EndTraderUtil;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.npc.WanderingTrader;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 public class EndTraderEntity extends AbstractTraderEntity {
 
+    public UUID linkedPlayerUuid = null;
+    public String linkedPlayerName = null;
     public EndTraderEntity(EntityType<? extends WanderingTrader> entityType, Level level) {
         super(entityType, level);
+    }
+
+    @Override
+    public @NotNull InteractionResult mobInteract(Player player, InteractionHand hand) {
+        if (!isClientSide()) {
+            if (linkedPlayerUuid == null) {
+                linkedPlayerUuid = player.getUUID();
+                linkedPlayerName = player.getName().getString();
+            }
+
+            if (linkedPlayerUuid != null) {
+                getOffers();
+                this.offers.removeIf(MerchantOffer::isOutOfStock);
+                offers = EndTraderUtil.createOffersFromLostLoot(linkedPlayerUuid, offers);
+            }
+        }
+        return super.mobInteract(player, hand);
     }
 
 }
