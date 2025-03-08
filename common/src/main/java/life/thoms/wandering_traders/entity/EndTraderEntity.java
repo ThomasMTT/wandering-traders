891d073c1de49c717090b93c736081ee195caa51
ThomasMTT <thomasmatthys@protonmail.com>
2025-03-08 01:56:51 +0100

    [FEATURE] add actionBar messages related to trades

--- common/src/main/java/life/thoms/wandering_traders/entity/EndTraderEntity.java
+++ common/src/main/java/life/thoms/wandering_traders/entity/EndTraderEntity.java
@@ -1,6 +1,9 @@
 package life.thoms.wandering_traders.entity;
 
 import life.thoms.wandering_traders.util.trader.EndTraderUtil;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
@@ -31,7 +34,15 @@ public class EndTraderEntity extends AbstractTraderEntity {
             if (linkedPlayerUuid != null) {
                 getOffers();
                 this.offers.removeIf(MerchantOffer::isOutOfStock);
-                offers = EndTraderUtil.createOffersFromLostLoot(linkedPlayerUuid, offers);
+                if (linkedPlayerUuid == player.getUUID()) {
+                    offers = EndTraderUtil.createOffersFromLostLoot(linkedPlayerUuid, offers);
+                    if (offers.isEmpty()) {
+                        player.displayClientMessage(Component.translatable("end_trader_message.no_trades"), true);
+                    }
+                } else {
+                    MutableComponent message = Component.translatable("end_trader_message.only_trade_with");
+                    player.displayClientMessage(message.append(linkedPlayerName).withStyle(ChatFormatting.ITALIC), true);
+                }
             }
         }
         return super.mobInteract(player, hand);
