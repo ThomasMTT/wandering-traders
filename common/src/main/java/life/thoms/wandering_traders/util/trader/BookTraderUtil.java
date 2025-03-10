f571a906e01f8571020e77de904e5fa16cbc219a
ThomasMTT <thomasmatthys@protonmail.com>
2025-03-10 02:49:07 +0100

    [FEATURE] create book trader trades

---
+++ common/src/main/java/life/thoms/wandering_traders/util/trader/BookTraderUtil.java
@@ -0,0 +1,71 @@
+package life.thoms.wandering_traders.util.trader;
+
+import life.thoms.wandering_traders.util.MerchantUtil;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
+import net.minecraft.world.item.trading.ItemCost;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.item.trading.MerchantOffers;
+
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class BookTraderUtil {
+
+    public static final Random RANDOM = new Random();
+
+    public static MerchantOffers generateOffers() {
+        MerchantOffers offers = new MerchantOffers();
+        offers.clear();
+
+        for (int i = 0; i < 6; i++) {
+            offers.add(createOffer());
+        }
+        offers.sort((x, z) -> {
+            if (x.getItemCostA().count() < z.getItemCostA().count()) {
+                return 1;
+            } else if (x.getItemCostA().count() == z.getItemCostA().count()) {
+                return 0;
+            } else {
+                return -1;
+            }
+        });
+        return offers;
+    }
+
+    private static MerchantOffer createOffer() {
+        ItemStack enchantedBook = generateStackFromItem(RANDOM);
+        ItemEnchantments enchantments = enchantedBook.getComponents().get(DataComponents.STORED_ENCHANTMENTS);
+        AtomicInteger levels = new AtomicInteger();
+        enchantments.entrySet().forEach(entry -> {
+            levels.addAndGet(enchantments.getLevel(entry.getKey().value()));
+        });
+        return new MerchantOffer(
+                new ItemCost(Items.EMERALD, enchantments.size() * 5 + levels.get() * 3),
+                Optional.of(new ItemCost(Items.BOOK)),
+                enchantedBook,
+                RANDOM.nextInt(1, 4), 1, 1
+        );
+    }
+
+    public static ItemStack generateStackFromItem(Random random) {
+        ItemStack stack = new ItemStack(Items.ENCHANTED_BOOK);
+
+        // Determine if item should be enchanted and how many times
+        int enchantmentChance = random.nextInt(100);
+        int enchantments = 1;
+        if (enchantmentChance > 90) enchantments = 3;
+        else if (enchantmentChance > 75) enchantments = 2;
+
+        // Apply random compatible enchantments (max 20 tries per enchantment (if reached returns the item as is)
+        while (stack.getComponents().get(DataComponents.STORED_ENCHANTMENTS).size() < enchantments) {
+            Enchantment enchantment = MerchantUtil.ENCHANTMENTS.get(random.nextInt(MerchantUtil.ENCHANTMENTS.size()));
+            stack.enchant(enchantment, RANDOM.nextInt(enchantment.getMaxLevel()));
+        }
+        return stack;
+    }
+
+}
