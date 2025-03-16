fcabe3e84ca27c3adcf9bfc9e7277d84b373ebce
ThomasMTT <thomasmatthys@protonmail.com>
2025-03-16 20:37:49 +0100

    [REFACTOR] ExclusiveTraderUtil

--- common/src/main/java/life/thoms/wandering_traders/util/trader/ExclusiveTraderUtil.java
+++ common/src/main/java/life/thoms/wandering_traders/util/trader/ExclusiveTraderUtil.java
@@ -12,30 +12,21 @@ import java.util.*;
 public class ExclusiveTraderUtil {
 
     public static final Random RANDOM = new Random();
-
-
+    private static final int MAX_USES = 1;
 
     public static MerchantOffers generateOffers() {
         MerchantOffers offers = new MerchantOffers();
         offers.clear();
 
         for (int i = 0; i < 6; i++) {
-            offers.add(createOffer());
+            ItemStack stack = new ItemStack(MerchantUtil.EXCLUSIVE_ITEMS_LIST.get(
+                    RANDOM.nextInt(MerchantUtil.EXCLUSIVE_ITEMS_LIST.size())));
+
+            int price = MerchantUtil.EXCLUSIVE_ITEMS.get(stack.getItem());
+            offers.add(MerchantUtil.createOffer(stack, price, MAX_USES));
         }
         offers.sort((x, z) -> Integer.compare(z.getItemCostA().count(), x.getItemCostA().count()));
         return offers;
-    }
-
-    private static MerchantOffer createOffer() {
-        ItemStack stack = new ItemStack(MerchantUtil.EXCLUSIVE_ITEMS_LIST.get(
-                RANDOM.nextInt(MerchantUtil.EXCLUSIVE_ITEMS_LIST.size())));
-        int price = MerchantUtil.EXCLUSIVE_ITEMS.get(stack.getItem());
-
-        return new MerchantOffer(
-                new ItemCost(Items.EMERALD, price),
-                stack,
-                1, 1, 1
-        );
     }
 
 }
