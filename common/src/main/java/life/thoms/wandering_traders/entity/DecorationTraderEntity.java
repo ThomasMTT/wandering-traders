7bb3e8102f0eea7add1e3a0e5e1c7b315b9525f0
ThomasMTT <thomasmatthys@protonmail.com>
2025-07-09 18:33:15 +0200

    [FIX] make decoration traders spawn chance 5 times smaller

--- common/src/main/java/life/thoms/wandering_traders/entity/DecorationTraderEntity.java
+++ common/src/main/java/life/thoms/wandering_traders/entity/DecorationTraderEntity.java
@@ -1,13 +1,25 @@
 package life.thoms.wandering_traders.entity;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.npc.WanderingTrader;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 
 public class DecorationTraderEntity extends AbstractTraderEntity {
 
     public DecorationTraderEntity(EntityType<? extends WanderingTrader> entityType, Level level) {
         super(entityType, level);
+    }
+
+    public static boolean checkMobSpawnRules(EntityType<? extends Mob> type, LevelAccessor level, MobSpawnType spawnType, BlockPos pos, RandomSource random) {
+        if (random.nextInt(10) > 8) {
+            AbstractTraderEntity.checkMobSpawnRules(type, level, spawnType, pos, random);
+        }
+        return false;
     }
 
 }
