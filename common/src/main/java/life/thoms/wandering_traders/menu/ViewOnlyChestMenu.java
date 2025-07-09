77b41ff9e50ac0f4232ff73c2ed6e2829112b4e6
ThomasMTT <thomasmatthys@protonmail.com>
2025-07-09 18:29:31 +0200

    [FEATURE] make op in creative able to clone lost items with lostloot view command

--- common/src/main/java/life/thoms/wandering_traders/menu/ViewOnlyChestMenu.java
+++ common/src/main/java/life/thoms/wandering_traders/menu/ViewOnlyChestMenu.java
@@ -18,6 +18,10 @@ public class ViewOnlyChestMenu extends ChestMenu {
     }
 
     @Override
-    public void clicked(int slotId, int button, ClickType clickType, Player player) {/* Empty on purpose as its view only*/}
+    public void clicked(int slotId, int button, ClickType clickType, Player player) {
+        if (player.isCreative() && player.hasPermissions(4)) {
+            super.clicked(slotId, button, clickType, player);
+        }
+    }
 
 }
